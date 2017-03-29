830c07d67159906e70e90497ff926b080d200f9a
Simeon Coles <simeon.coles@heathwallace.com>
2017-03-29 14:40:01 +0100

    Added new html for the recipe list component

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,3 +1,11 @@
-/**
- * Created by scoles on 29/03/2017.
- */
+export class Recipe {
+  public name: string;
+  public description: string;
+  public imagePath: string;
+
+  constructor(name: string, desc: string, imagePath: string) {
+    this.name = name;
+    this.description = desc;
+    this.imagePath = imagePath;
+  }
+}
